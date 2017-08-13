073281726e45ea0a869b06a4e737e94549c344ed
vainotuisk <vainotuisk@gmail.com>
2017-08-13 22:08:58 +0300

    ümardus

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ font-size: 3em;
 `}</style>
     <Row>
         <Col xs="6" sm="4">Kiirus:  {Math.round(props.shows.wind.speed)}m/s</Col>
-        <Col xs="6" sm="4">Suund: {props.shows.wind.deg}</Col>
+        <Col xs="6" sm="4">Suund: {Math.round(props.shows.wind.deg)}</Col>
         <Col sm="4">Temp: {Math.round(props.shows.main.temp) -273} &deg;</Col>
     </Row>
 </div>
